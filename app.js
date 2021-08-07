552cce46d7918ecb61e1e040000c6bfe67727176
Huy Nguyen <n2huy94@gmail.com>
2021-08-07 14:30:23 +0700

    Test CICD

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello From Huy Nguyen!')
+  res.send('Hello From Huy Nguyen! Test CI/CD Updated 07/08')
 })
 
 app.listen(port, () => {
